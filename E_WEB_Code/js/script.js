cc229618be0b1e300f2e16ca57e862589a5448d3
joaopakina14 <pakina@live.com.pt>
2024-09-23 12:58:15 +0100

    df

---
+++ E_WEB_Code/js/script.js
@@ -0,0 +1,72 @@
+// alert("hello world");
+// alert("olá mundo");
+
+// let myCourse = "Front End Developer";
+// let myName = "João";
+// let mySurname = "Pimentel";
+
+// alert(myCourse);
+// alert ("Olá" + " "+ myName + " "+ mySurname);
+// let courseName = "Front-End Developer";
+
+// indica a posiçáo 10 da variável courseName
+// alert(courseName[10]);
+
+// Indica o numero de caracteres nesta variável
+// alert("O tamanho de "+courseName+" são "+courseName.length+" caracteres");
+
+// myCourse.trim().toUpperCase();
+
+// alert(myCourse.toUpperCase());
+
+
+// alert (typeof myCourse);
+// alert (Math.PI);
+
+// let userName = prompt("Qual o seu nome?");
+// alert (userName);
+
+//ARRAYS É UM CONJUNTO DE DADOS QUE A VARIÁVEL SE DEFINE PELOS PARENTESES RETOS E OS DADOS DENTRO DELES SAO SEPARADOS PELA VIRGULA [];
+let weekdays = ['segunda', 'terça', 'quarta', 'quinta', 'sexta', 4, true]
+
+// DECLARAÇÃO DE UM ARRAY VAZIO
+// let students = [];
+
+// //IMPRIMIR NA CONSOLA UMA POSIÇÃO DE UM ARRAY
+// alert(weekdays[1]);
+
+// alert(weekdays[6]);
+
+// //IMPRIMIR O NUMERO DE STRINGS NO ARRAY
+// alert (weekdays.length);
+
+// //ASSIGNAR DE UMA POSIÇÃO DO ARRAY POR EXEMPLO PARA DOMINGO
+// weekdays [7] = 'domingo';
+// alert (weekdays[7]);
+
+// //ADICIONAR DADOS AO FIM DA LISTA .PUSH
+// weekdays.push('nãoTenhoMaisDiasDaSemana');
+// alert (weekdays);
+
+// //REMOVER DADOS DO FIM DA LISTA
+// weekdays.pop([7]);
+// alert (weekdays);
+
+// //REMOVER DADOS DO ÍNICIO
+// weekdays.shift ([]);
+// alert (weekdays);
+
+// //ADICIONAR DADOS AO INICIO
+// weekdays.unshift ('Inicio');
+// alert (weekdays);
+
+
+
+
+
+
+
+
+
+
+
